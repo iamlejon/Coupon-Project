b83debec27c48555e55c7fd3bd82d63916bbcff6
iamlejon <niquelejon@gmail.com>
2023-02-23 17:52:22 -0600

    First Commit.  Coupon Service Project

---
+++ couponservice/src/main/java/com/learning/springcloud/controllers/CouponRestController.java
@@ -0,0 +1,31 @@
+package com.learning.springcloud.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learning.springcloud.model.Coupon;
+import com.learning.springcloud.repos.CouponRepo;
+
+@RestController
+@RequestMapping("/couponapi")
+public class CouponRestController {
+	
+	@Autowired
+	CouponRepo repo;
+	
+	@PostMapping("/coupons")
+	public Coupon create(@RequestBody Coupon coupon) {
+		return repo.save(coupon);
+	}
+	
+	@GetMapping("/coupons/{code}")
+	public Coupon getCoupon(@PathVariable("code") String code) {
+		return repo.findByCode(code);
+	}
+
+}
